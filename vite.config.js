51b4cc387d73a0ae77443b8b19f6ca651eeb9df1
Manuel Vergara <manuel@vergaracarmona.es>
2023-04-01 09:43:10 +0200

    Add base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://manuelver.github.io/',
+  base: 'https://manuelver.github.io/gh-page-manuelver/',
   plugins: [react()]
 })
